6f60d753093b4813156b4a428e84c736d6d935a3
SekasJr <semly.kase@gmail.com>
2021-07-31 22:44:05 +0800

    Membuat Form Tahun Akademik

--- app/Views/admin/v_ta.php
+++ app/Views/admin/v_ta.php
@@ -62,3 +62,43 @@
 
     </div>
 </div>
+
+<!-- /.modal add Tahun Akademik -->
+<div class="modal fade" id="add">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">Tambah <?= $title ?> </h4>
+            </div>
+            <div class="modal-body">
+                <?php 
+                echo form_open('ta/add');
+                ?>
+
+                <div class="form-group">
+                    <label>Tahun Akademik</label>
+                    <input name="ta" class="form-control" placeholder="Ex: 2020/2021 " required>
+                </div>
+                
+                <div class="form-group">
+                    <label>Semester</label>
+                    <select name="semester" class="form-control">
+                        <option value="Ganjil">Ganjil</option>
+                        <option value="Genap">Genap</option>
+                    </select>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-warning pull-left btn-flat" data-dismiss="modal">Kembali</button>
+                <button type="submit" class="btn btn-success btn-flat">Simpan</button>
+            </div>
+            <?php echo form_close() ?>
+        </div>
+    <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
